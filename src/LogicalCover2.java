17b43e73f877853e9418997376376f608cdbdb6d
Florent Bariod <fbariod@gmail.com>
2021-05-01 18:08:58 -0400

    multiple images instances

--- src/LogicalCover2.java
+++ src/LogicalCover2.java
@@ -23,10 +23,10 @@ public class LogicalCover2 extends PApplet {
 
     @Override
     public void draw() {
-        var ROTATIONS_MAX = 1000.0f;
-        var DIVISION = 4.0f;
+        var ROTATIONS_MAX = 100.0f;
+        var DIVISION = 2;
         var FRAME_SIZE = width;
-        var POINTS_MAX = 20;
+        var POINTS_MAX = 2000;
         var NOISE_SEED = 78950423890l;
         var NOISE_X_QUOTIENT = 0.4f;
         var NOISE_Y_QUOTIENT = 0.2f;
@@ -37,44 +37,74 @@ public class LogicalCover2 extends PApplet {
         // draw background
         blendMode(BLEND);
 //        background(0, 0, 0);
-        background(217,72,76);
+//        background(217,72,76);
 //        background(bg);
 
-        translate(width / 2.0f, height / 2.0f);
+//        translate(width / 2.0f, height / 2.0f);
 //        noFill();
-        noStroke();
+//        noStroke();
         blendMode(BLEND);
 
-        for (int rotationCount = 0; rotationCount < ROTATIONS_MAX; rotationCount++) {
-//            stroke(map(rotationCount, 0, ROTATIONS_MAX, 0, 360),
-//                    map(rotationCount, 0, ROTATIONS_MAX, 0, 100),
-//                    map(rotationCount, 0, ROTATIONS_MAX, 0, 100),
-//                    60);
-            fill(map(rotationCount, 0, ROTATIONS_MAX, 0, 50),
-                    map(rotationCount, 0, ROTATIONS_MAX, 0, 70),
-                    map(rotationCount, 0, ROTATIONS_MAX, 0, 100),
+
+        for (int i = 0; i < DIVISION / 2; i++) {
+            pushMatrix();
+            translate(i * (width / (DIVISION / 2)), 0);
+            for (var j = 0; j < DIVISION / 2; j++) {
+                pushMatrix();
+                translate(0, j * (height / (DIVISION / 2)));
+                InputParameters inputParameters = new InputParameters();
+                inputParameters.frameSize = FRAME_SIZE / (DIVISION / 2);
+                inputParameters.noiseSeed = NOISE_SEED + i + j;
+                inputParameters.rotationsMax = ROTATIONS_MAX;
+                inputParameters.noiseXRatio = NOISE_X_QUOTIENT;
+                inputParameters.noiseYRatio = NOISE_Y_QUOTIENT;
+                inputParameters.pointsMax = POINTS_MAX;
+                inputParameters.h = 217;
+                inputParameters.s = 72;
+                inputParameters.b = 76;
+                drawInstance(inputParameters);
+                popMatrix();
+            }
+            popMatrix();
+        }
+    }
+
+    private void drawInstance(InputParameters input) {
+        noiseSeed(input.noiseSeed);
+        translate(input.frameSize / 2.0f, input.frameSize / 2.0f);
+        noStroke();
+        fill(input.h, input.s, input.b);
+        square(0, 0, input.frameSize);
+
+        for (int rotationCount = 0; rotationCount < input.rotationsMax; rotationCount++) {
+            stroke(map(rotationCount, 0, input.rotationsMax, 0, 50),
+                    map(rotationCount, 0, input.rotationsMax, 0, 70),
+                    map(rotationCount, 0, input.rotationsMax, 0, 100),
+                    60);
+            fill(map(rotationCount, 0, input.rotationsMax, 0, 50),
+                    map(rotationCount, 0, input.rotationsMax, 0, 70),
+                    map(rotationCount, 0, input.rotationsMax, 0, 100),
                     3);
             pushMatrix();
-            var rotateRatio = map(rotationCount, 0, ROTATIONS_MAX, 0.0f, 1.0f);
+            var rotateRatio = map(rotationCount, 0, input.rotationsMax, 0.0f, 1.0f);
             rotate(TWO_PI * rotateRatio);
-            var radiusBase = FRAME_SIZE * 0.5f * rotateRatio;
-            PShape ellipse = this.createShape(31, new float[]{0.0F, 0.0F, FRAME_SIZE * 0.9f * rotateRatio, FRAME_SIZE * 0.045f});
-            this.shape(ellipse);
+            var radiusBase = input.frameSize * 0.5f * rotateRatio;
+//            PShape ellipse = this.createShape(31, new float[]{0.0F, 0.0F, frameSize * 0.9f * rotateRatio, frameSize * 0.045f});
+//            this.shape(ellipse);
 
-//            beginShape();
-//            for (int pointsCount = 0; pointsCount < POINTS_MAX; ++pointsCount) {
-//                float pointRatio = map(pointsCount, 0, POINTS_MAX, 0.0f, 1.0f);
-//                float noiseValX = map(noise(pointsCount * NOISE_X_QUOTIENT * rotationCount), 0.0f, 1.0f, 0.6f, 1.4f);
-//                float noiseValY = map(noise(pointsCount * NOISE_Y_QUOTIENT * rotationCount), 0.0f, 1.0f, 0.6f, 1.4f);
-//                float x = radiusBase * noiseValX * rotateRatio * cos(TWO_PI * pointRatio);
-//                float y = radiusBase * noiseValY * rotateRatio * sin(TWO_PI * pointRatio);
-//                vertex(x, y);
-//            }
-//            endShape(CLOSE);
+            beginShape();
+            for (int pointsCount = 0; pointsCount < input.pointsMax; ++pointsCount) {
+                float pointRatio = map(pointsCount, 0, input.pointsMax, 0.0f, 1.0f);
+                float noiseValX = map(noise(pointsCount * input.noiseXRatio * rotationCount), 0.0f, 1.0f, 0.6f, 1.4f);
+                float noiseValY = map(noise(pointsCount * input.noiseYRatio * rotationCount), 0.0f, 1.0f, 0.6f, 1.4f);
+                float x = radiusBase * noiseValX * rotateRatio * cos(TWO_PI * pointRatio);
+                float y = radiusBase * noiseValY * rotateRatio * sin(TWO_PI * pointRatio);
+                vertex(x, y);
+            }
+            endShape(CLOSE);
 
             popMatrix();
         }
-
     }
 
     @Override
@@ -85,6 +115,18 @@ public class LogicalCover2 extends PApplet {
     public static void main(String... args) {
         LogicalCover2 pt = new LogicalCover2();
         PApplet.runSketch(new String[]{"LogicalCover2"}, pt);
+    }
+
+    class InputParameters {
+        float rotationsMax;
+        int frameSize;
+        long noiseSeed;
+        int pointsMax;
+        float noiseXRatio;
+        float noiseYRatio;
+        int h;
+        int s;
+        int b;
     }
 }
 
